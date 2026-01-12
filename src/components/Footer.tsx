c6794ba5bedca55bb22c5c79d32caa2885ccfe51
wombito <haydeejbb@gmail.com>
2026-01-12 14:07:45 -0300

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+function Footer() {
+    return (<div className="w-fit mx-auto text-white text-center text-sm lg:text-lg bg-slate-950/80 px-3 py-0.5 rounded-xl mb-1">© 2025 Fauna Chilena. Todos los derechos reservados.</div>)
+}
+
+export default Footer;
