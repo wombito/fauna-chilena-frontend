639503b935b418ada2c2beba8f254e52058cd13f
wombito <haydeejbb@gmail.com>
2026-01-12 14:03:52 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
+    <>
+    <div><Header/></div>
     <div>hola</div>
+    </>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="flex justify-center">
+      <h1 className="font-sans text-3xl lg:text-5xl text-center text-white m-10 p-2 lg:p-4 transition hover:bg-green-600/50 basis-100 rounded-4xl border-4 border-white border-double">
+        Fauna Chilena
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
