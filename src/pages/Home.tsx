14ba5a4bc113c40b601619882c79bb84c33d9aad
wombito <haydeejbb@gmail.com>
2026-01-12 14:25:43 -0300

    feat: add Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+function Home() {
+    <>
+    <Header/>
+    <Footer/>
+    </>
+}
+
+export default Home;
